c11e280f5194c03a3bee45725ae297693c5d14ec
hyunatic <habbo_armageddon@hotmail.com>
2020-10-20 15:07:24 +0800

    UPdate

--- SE_master/urls.py
+++ SE_master/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path 
+from django.urls import path, include
 from backend import views
 
 
@@ -32,6 +32,7 @@ urlpatterns = [
 
     path('api/create_User', views.create_User),
     path('api/login/',views.login),
+    path('', include('backend.urls'))
 
    
 ]
