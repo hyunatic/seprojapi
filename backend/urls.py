8e996ad901d6d8c58f0eafb5954f7aac0dc3a7e8
hyunatic <habbo_armageddon@hotmail.com>
2020-10-23 18:39:43 +0800

    sd

--- backend/urls.py
+++ backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path 
 from backend import views
-from backend import email as Verifyaccount 
+from backend import Verifyaccount 
 
 urlpatterns = [
 
