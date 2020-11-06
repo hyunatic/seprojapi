adb4f41e89866db2fe05ae807bbbf5fc2065f748
yonkatian <yonkatian@gmail.com>
2020-11-06 22:33:25 +0800

    Delete Create_Table_Query.sql

--- Create_Table_Query.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "Post" (
-	"Postid"	INTEGER,
-	"Userid"	INTEGER,
-	"ItemName"	BLOB,
-	"Category"	TEXT,
-	"Description"	TEXT,
-	"ImageId"	TEXT,
-	PRIMARY KEY("Postid" AUTOINCREMENT),
-	FOREIGN KEY("Userid") REFERENCES auth_user(id) );
